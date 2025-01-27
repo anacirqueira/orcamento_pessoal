a72f949eaef8cdc59e7e1d0fb9b02e2660e0dbc0
anacirqueira <cirqueiraana6@gmail.com>
2025-01-27 16:56:36 -0300

    Onsubmit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 (function main() {
-    
+    var obterElementos = function() {
+        var formReceita = document.getElementById('form-adicionar-receita');
+        var formDespesa = document.getElementById('form-adicionar-despesa');
+        return {
+            receita: {
+                form: formReceita
+            },
+            despesa: {
+                form: formDespesa
+            }
+
+        }
+    }
+
+    var elementos = obterElementos();
+
+    elementos.receita.form.onsubmit = function(event) {
+        event.preventDefault();
+        alert('Receita salva com sucesso!');
+    }
+
+    elementos.despesa.form.onsubmit = function(event) {
+        event.preventDefault();
+        alert('Despesa salva com sucesso!');
+    }
+
 })();
